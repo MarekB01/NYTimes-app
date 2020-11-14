fc7fd0415526745995b05412d4ec47e8f00d9382
Markian <markijanchyk@gmail.com>
2020-11-14 12:15:35 +0200

    first commit

---
+++ lib/strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  static const String appName = "nytimes_app";
+  static const String apiKey = "RoIGmzbGhPlHyLIKSBKqhgGlphYtOzPG";
+  static const String requestFailed = "Request Failed";
+  static const String retry = "RETRY";
+  static const String readMore = "READ MORE";
+}
